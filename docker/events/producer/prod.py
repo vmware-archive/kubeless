76217b6310f945fb04dd30b5d8f7c2727379479f
sebgoa <runseb@gmail.com>
2016-12-04 17:02:23 +0100

    fix producer to emit event with kafka-controller

--- docker/events/producer/prod.py
+++ docker/events/producer/prod.py
@@ -3,6 +3,7 @@
 import json
 from kafka import KafkaProducer
 
-producer=KafkaProducer(bootstrap_servers='10.0.0.110:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-producer.send('foobar',{'hello':'world'})
+producer=KafkaProducer(bootstrap_servers='kafka:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+while True:
+    producer.send('foobar',{'hello':'world'})
 
