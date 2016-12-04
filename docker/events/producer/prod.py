a0c4ab783925b3de19afd4ddb23467294abaa1c0
sebgoa <runseb@gmail.com>
2016-12-04 17:28:31 +0100

    Switch kafka to kubeless namespace

--- docker/events/producer/prod.py
+++ docker/events/producer/prod.py
@@ -3,7 +3,7 @@
 import json
 from kafka import KafkaProducer
 
-producer=KafkaProducer(bootstrap_servers='kafka:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer=KafkaProducer(bootstrap_servers='kafka.kubeless:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 while True:
     producer.send('foobar',{'hello':'world'})
 
