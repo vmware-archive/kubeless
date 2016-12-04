03c2ecca10f43acc0b6a0839cfd2acac98ccc1a5
Sebastien Goasguen <runseb@gmail.com>
2016-12-04 16:36:01 +0100

    start building event processor runtime

--- docker/events/consumer/con.py
+++ docker/events/consumer/con.py
@@ -3,7 +3,7 @@
 import json
 from kafka import KafkaConsumer
 
-consumer=KafkaConsumer(bootstrap_servers='10.0.0.110:9092',value_deserializer=json.dumps)
+consumer=KafkaConsumer(bootstrap_servers='kafka:9092',value_deserializer=json.dumps)
 consumer.subscribe(['foobar'])
 while True:
     for msg in consumer:
