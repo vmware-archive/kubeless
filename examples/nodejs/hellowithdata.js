1e77ebbcbd2476dca411afd70bee17a229fa394f
Ross Kukulinski <ross@kukulinski.com>
2017-03-10 14:26:38 -0800

    Fix nodejs with data example

--- examples/nodejs/hellowithdata.js
+++ examples/nodejs/hellowithdata.js
@@ -1,6 +1,14 @@
 module.exports = {
   handler: function (req, res) {
-    console.log(req.body)
-    res.end('hello world')
+    var body = []
+    req.on('error', function (err) {
+      console.error(err)
+    }).on('data', function (chunk) {
+      body.push(chunk)
+    }).on('end', function () {
+      body = Buffer.concat(body).toString()
+      console.log(body)
+      res.end(body)
+    })
   }
 }
