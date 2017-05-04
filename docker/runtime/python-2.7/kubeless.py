b16c0630268cc95b8663c6dd1e38e9ff89f9c338
Angus Lees <gus@inodes.org>
2017-05-04 12:56:40 +1000

    Add prometheus /metrics endpoint to python runtime
    Exports basic function_duration_seconds histogram, and
    function_{calls,failures}_total counters, all labelled with HTTP
    method.

--- docker/runtime/python-2.7/kubeless.py
+++ docker/runtime/python-2.7/kubeless.py
@@ -4,6 +4,7 @@ import os
 import imp
 
 import bottle
+import prometheus_client as prom
 
 mod = imp.load_source('function',
                       '/kubeless/%s.py' % os.getenv('MOD_NAME'))
@@ -11,17 +12,36 @@ func = getattr(mod, os.getenv('FUNC_HANDLER'))
 
 app = application = bottle.app()
 
-@app.get('/')
-def handler():
-    return func()
+func_hist = prom.Histogram('function_duration_seconds',
+                           'Duration of user function in seconds',
+                           ['method'])
+func_calls = prom.Counter('function_calls_total',
+                           'Number of calls to user function',
+                          ['method'])
+func_errors = prom.Counter('function_failures_total',
+                           'Number of exceptions in user function',
+                           ['method'])
 
-@app.post('/')
-def post_handler():
-    return func(bottle.request)
+@app.route('/', method=['GET', 'POST'])
+def handler():
+    req = bottle.request
+    method = req.method
+    func_calls.labels(method).inc()
+    with func_errors.labels(method).count_exceptions():
+        with func_hist.labels(method).time():
+            if method == 'GET':
+                return func()
+            else:
+                return func(bottle.request)
 
 @app.get('/healthz')
 def healthz():
     return 'OK'
+
+@app.get('/metrics')
+def metrics():
+    bottle.response.content_type = prom.CONTENT_TYPE_LATEST
+    return prom.generate_latest(prom.REGISTRY)
 
 if __name__ == '__main__':
     import logging
