2a3038dfcb25b1245fbe4f0f9d9fe1e2f08d3345
Jorge Bianquetti <jbianquetti@bitnami.com>
2017-05-26 16:56:15 +0200

    added readmes and fixed naming convention

--- manifests/kafka/run.sh
+++ manifests/kafka/run.sh
@@ -1,4 +1,4 @@
 kubectl create -f namespace.yaml
-kubectl create -f kafka.headless.svc.yaml
-kubectl create -f kafka.svc.yaml
+kubectl create -f kafka-headless-svc.yaml
+kubectl create -f kafka-svc.yaml
 kubectl create -f kafka-deployment.yaml
