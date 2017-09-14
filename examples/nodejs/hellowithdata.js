2a9ddbf6141feb09d1e612a17a2198068ce6144a
Andres <andres@bitnami.com>
2017-09-14 09:58:11 +0200

    Fix and simplify hellowithdata.js

--- examples/nodejs/hellowithdata.js
+++ examples/nodejs/hellowithdata.js
@@ -1,17 +1,6 @@
 module.exports = {
   handler: (req, res) => {
-    let body = [];
-    return new Promise((resolve, reject) => {
-      req.on('error', (err) => {
-        reject(new Error(err));
-      }).on('data', (chunk) => {
-        body.push(chunk);
-      }).on('end', () => {
-        body = Buffer.concat(body).toString();
-        console.log(body);
-        res.end(body);
-        resolve();
-      });
-    });
+    console.log(req.body);
+    res.end(JSON.stringify(req.body));
   },
 };
