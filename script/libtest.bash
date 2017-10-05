a16710de57e6feea159af9becfbf9c91a474e493
Andres <andres@bitnami.com>
2017-10-05 11:44:48 +0200

    Test with some sleeps

--- script/libtest.bash
+++ script/libtest.bash
@@ -259,6 +259,7 @@ test_kubeless_function_update() {
     echo_info "UPDATE: $func"
     make -sC examples ${func}-update
     k8s_wait_for_uniq_pod -l function=${func}
+    sleep 10
     make -sC examples ${func}-update-verify
 }
 # vim: sw=4 ts=4 et si
