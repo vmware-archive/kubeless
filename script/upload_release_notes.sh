bfefb404e22a209659ef06b98a51051d72dd5c21
Andres <andres@bitnami.com>
2017-10-11 15:03:34 +0200

    Fix upload_release_notes script

--- script/upload_release_notes.sh
+++ script/upload_release_notes.sh
@@ -15,39 +15,40 @@ function commit_list {
 
 function get_release_notes {
   commits=`commit_list $tag`
-  notes=$(cat << EOF
-This release includes the following commits and features:\n
-$commits\n
-To install this latest version, use the manifest that is part of the release:
-
-**NO RBAC:**
-
-\`\`\`console
-kubectl create ns kubeless
-curl -sL https://github.com/kubeless/kubeless/releases/download/$tag/kubeless-$tag.yaml | kubectl create -f -
-\`\`\`
-
-**WITH RBAC ENABLED:**
-
-\`\`\`console
-kubectl create ns kubeless
-curl -sL https://github.com/kubeless/kubeless/releases/download/$tag/kubeless-rbac-$tag.yaml | kubectl create -f -
-\`\`\`
-EOF )
-  echo -e "${notes}"
+  notes=$(echo "\
+This release includes the following commits and features:\\n\
+$commits\\n\\n\
+To install this latest version, use the manifest that is part of the release:\\n\
+\\n\
+**NO RBAC:**\\n\
+\\n\
+\`\`\`console\\n\
+kubectl create ns kubeless\\n\
+curl -sL https://github.com/kubeless/kubeless/releases/download/$tag/kubeless-$tag.yaml | kubectl create -f -\\n\
+\`\`\`\\n\
+\\n\
+**WITH RBAC ENABLED:**\\n\
+\\n\
+\`\`\`console\\n\
+kubectl create ns kubeless\\n\
+curl -sL https://github.com/kubeless/kubeless/releases/download/$tag/kubeless-rbac-$tag.yaml | kubectl create -f -\\n\
+\`\`\`\\n\
+")
+  echo "${notes}"
 }
 
 function release_tag {
   local tag=$1
   local get_release_notes=$(get_release_notes $tag)
-  release=`curl -H "Authorization: token $ACCESS_TOKEN" --request PATCH -s --data "{
+  local release_id=$(curl -s https://api.github.com/repos/$REPO_DOMAIN/$REPO_NAME/releases | jq  --raw-output '.[0].id')
+  local release=`curl -H "Authorization: token $ACCESS_TOKEN" --request PATCH --data "{
     \"tag_name\": \"$tag\",
     \"target_commitish\": \"master\",
     \"name\": \"$tag\",
-    \"body\": \"Release $tag includes the following commits: \n$release_notes\",
+    \"body\": \"$release_notes\",
     \"draft\": true,
     \"prerelease\": false
-  }" https://api.github.com/repos/$REPO_DOMAIN/$REPO_NAME/releases`
+  }" https://api.github.com/repos/$REPO_DOMAIN/$REPO_NAME/releases/$release_id`
   echo $release | jq ".id"
 }
 
