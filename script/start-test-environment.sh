8b38398ba2db06b48ae3179ed7e5b7bca9a19e70
Andres <andres@bitnami.com>
2017-11-29 10:32:48 +0100

    Force minikube version. Improve error message

--- script/start-test-environment.sh
+++ script/start-test-environment.sh
@@ -8,9 +8,17 @@ ROOTDIR=`cd $(dirname $SCRIPT)/.. && pwd`
 
 COMMAND="${@:-bash}"
 
+if ! minikube version | grep -q "v0.22.3"; then
+  # Until #399 is fixed
+  echo "Only minikube v0.22.3 is supported"
+  exit 1
+fi
+
 if ! minikube status | grep -q "minikube: $"; then
-    echo "Unable to start the test environment with an existing instance of minikube"
-    exit 1
+  echo "Unable to start the test environment with an existing instance of minikube"
+  echo "Delete the current profile executing 'minikube delete' or create a new one"
+  echo "executing 'minikube profile new_profile'"
+  exit 1
 fi
 
 minikube start --extra-config=apiserver.Authorization.Mode=RBAC
