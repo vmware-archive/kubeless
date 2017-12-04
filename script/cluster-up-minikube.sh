9cb886f21549c540d809d079805bf75d01184efa
Andres <andres@bitnami.com>
2017-12-04 16:29:49 +0100

    Download nsenter instead of building it

--- script/cluster-up-minikube.sh
+++ script/cluster-up-minikube.sh
@@ -36,24 +36,10 @@ install_bin() {
 # (--vm-driver=none, runs dockerized)
 check_or_build_nsenter() {
     which nsenter >/dev/null && return 0
-    echo "INFO: Building 'nsenter' ..."
-cat <<-EOF | docker run -i --rm -v "$(pwd):/build" ubuntu:14.04 >& nsenter.build.log
-        apt-get update
-        apt-get install -qy git bison build-essential autopoint libtool automake autoconf gettext pkg-config
-        git clone --depth 1 git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git /tmp/util-linux
-        cd /tmp/util-linux
-        ./autogen.sh
-        ./configure --without-python --disable-all-programs --enable-nsenter
-        make nsenter
-        cp -pfv nsenter /build
-EOF
-    if [ ! -f ./nsenter ]; then
-        echo "ERROR: nsenter build failed, log:"
-        cat nsenter.build.log
-        return 1
-    fi
-    echo "INFO: nsenter build OK, installing ..."
-    install_bin ./nsenter
+    echo "INFO: Getting 'nsenter' ..."
+    curl -LO http://mirrors.kernel.org/ubuntu/pool/main/u/util-linux/util-linux_2.30.1-0ubuntu4_amd64.deb
+    dpkg -x ./util-linux_2.30.1-0ubuntu4_amd64.deb /tmp/out
+    install_bin /tmp/out/usr/bin/nsenter
 }
 check_or_install_minikube() {
     which minikube || {
