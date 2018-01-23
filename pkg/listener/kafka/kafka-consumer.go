d2c915a96b439417889970bdd3c87163d4604fb2
Tuna <ng.tuna@gmail.com>
2018-01-23 17:57:07 +0100

    group consumer via function_id , support comma list of topics

--- pkg/listener/kafka/kafka-consumer.go
+++ pkg/listener/kafka/kafka-consumer.go
@@ -1,64 +1,75 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
-	"strconv"
 	"strings"
+	"syscall"
 
 	"github.com/Shopify/sarama"
+	"github.com/bsm/sarama-cluster"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	brokers := os.Getenv("BROKER_LIST")
 	if brokers == "" {
-		logrus.Fatalf("broker can't be empty. Please set env BROKER_LIST.")
-	}
-	brokerList := strings.Split(brokers, ",")
-	topic := os.Getenv("TOPIC")
-	if topic == "" {
-		logrus.Fatalf("topic can't be empty. Please set env TOPIC.")
-	}
-	partition := os.Getenv("PARTITION")
-	if partition == "" {
-		partition = "0"
+		logrus.Fatalf("The comma separated list of brokers can't be empty. Please set it in env BROKER_LIST.")
 	}
 
-	config := sarama.NewConfig()
+	topics := os.Getenv("TOPICS")
+	if topics == "" {
+		logrus.Fatalf("The comma separated list of topics can't be empty. Please set env TOPICS.")
+	}
+
+	functionID := os.Getenv("FUNCTION_ID")
+	if functionID == "" {
+		logrus.Fatalf("Function_ID is string concatenation of `function_name + namespace`, and it can't be empty. Please set env FUNCTION_ID")
+	}
+
+	// Init config
+	config := cluster.NewConfig()
+
 	config.Consumer.Return.Errors = true
-	master, err := sarama.NewConsumer(brokerList, config)
+	config.Group.Return.Notifications = true
+	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+
+	// Init consumer, consume errors & messages
+	// consumer is grouped and labeled by functionID to receive load-balanced messages
+	// More details: https://kafka.apache.org/documentation/#intro_consumers
+	consumer, err := cluster.NewConsumer(strings.Split(brokers, ","), functionID, strings.Split(topics, ","), config)
 	if err != nil {
-		logrus.Fatal(err)
+		logrus.Fatalf("Failed to start consumer: %v", err)
 	}
-	defer func() {
-		if err := master.Close(); err != nil {
-			logrus.Fatal(err)
-		}
-	}()
-	p, err := strconv.Atoi(partition)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	consumer, err := master.ConsumePartition(topic, int32(p), sarama.OffsetOldest)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-	doneCh := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case err := <-consumer.Errors():
-				logrus.Fatal(err)
-			case msg := <-consumer.Messages():
-				logrus.Infof("Received messages %s %s", string(msg.Key), string(msg.Value))
-			case <-signals:
-				logrus.Fatalf("Interrupt is detected")
-				doneCh <- struct{}{}
+	defer consumer.Close()
+
+	// Create signal channel
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
+
+	// Consume all channels, wait for signal to exit
+	for {
+		select {
+		case msg, more := <-consumer.Messages():
+			if more {
+				fmt.Printf("Partition:\t%d\n", msg.Partition)
+				fmt.Printf("Offset:\t%d\n", msg.Offset)
+				fmt.Printf("Key:\t%s\n", string(msg.Key))
+				fmt.Printf("Value:\t%s\n", string(msg.Value))
+				fmt.Println()
+				consumer.MarkOffset(msg, "")
 			}
+		case ntf, more := <-consumer.Notifications():
+			if more {
+				logrus.Infof("Rebalanced: %+v\n", ntf)
+			}
+		case err, more := <-consumer.Errors():
+			if more {
+				logrus.Fatalf("Error: %s\n", err.Error())
+			}
+		case <-sigchan:
+			return
 		}
-	}()
-	<-doneCh
+	}
 }
