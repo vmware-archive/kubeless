fad49ce476563cba289e7f61a2bcbe1efe0cd2e8
Paolo Mainardi <paolo.mainardi@sparkfabrik.com>
2018-02-04 15:38:23 +0100

    add an example using dependencies

---
+++ examples/php/helloGetWithDeps.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+function foo() {
+  // create a log channel
+  $log = new Logger('name');
+  $log->pushHandler(new StreamHandler("php://stdout", Logger::INFO));
+
+  // add records to the log
+  $log->info('Hello');
+  $log->info('World');
+}
