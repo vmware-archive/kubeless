0f59e7a1a18512ddd12e3d1cb9facd6d5ac7b9e4
Christoph Scheiber <christoph.scheiber@sap.com>
2018-03-13 19:25:21 +0100

    nodejs, remove runtime internal data from sandbox, avoid code concat

--- docker/runtime/nodejs/kubeless.js
+++ docker/runtime/nodejs/kubeless.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const fs = require('fs');
+const vm = require('vm');
+const path = require('path');
+const Module = require('module');
+
 const bodyParser = require('body-parser');
 const client = require('prom-client');
 const express = require('express');
@@ -9,7 +14,7 @@ const morgan = require('morgan');
 const app = express();
 app.use(morgan('combined'));
 const bodParserOptions = {
-  type: '*/*'
+    type: '*/*'
 };
 app.use(bodyParser.raw(bodParserOptions));
 
@@ -18,89 +23,114 @@ const funcHandler = process.env.FUNC_HANDLER;
 const timeout = Number(process.env.FUNC_TIMEOUT || '180');
 const funcPort = Number(process.env.FUNC_PORT || '8080');
 
-const statistics = helper.prepareStatistics('method', client);
+const modRootPath = require.main.filename.replace('kubeless.js', 'kubeless');
+const modPath = path.join(modRootPath, `${modName}.js`);
+const libPath = path.join(modRootPath, 'node_modules');
+const pkgPath = path.join(modRootPath, 'package.json');
+const libDeps = helper.readDependencies(pkgPath);
+
+const { timeHistogram, callsCounter, errorsCounter } = helper.prepareStatistics('method', client);
 helper.routeLivenessProbe(app);
 helper.routeMetrics(app, client);
-const functionCallingCode = `
-try {
-  Promise.resolve(module.exports.${funcHandler}(event, context)).then((result) => {
-    switch(typeof result) {
-      case 'string':
-        _res.end(result);
-        break;
-      case 'object':
-        _res.json(result);
-        break;
-      default:
-        _res.end(JSON.stringify(result))
+
+const script = new vm.Script(fs.readFileSync(modPath), {
+    filename: modPath,
+    displayErrors: true,
+});
+
+function modRequire(p, req, res, end) {
+    if (p.indexOf('./') === 0)
+        return require(path.join(path.dirname(modPath), p));
+    else if (libDeps.includes(p))
+        return require(path.join(libPath, p));
+    else if (p === 'kubeless')
+        return (handler) => modExecute(handler, req, res, end);
+    else
+        return require(p);
+}
+
+function modExecute(handler, req, res, end) {
+    try {
+        const event = {
+            'event-type': req.get('event-type'),
+            'event-id': req.get('event-id'),
+            'event-time': req.get('event-time'),
+            'event-namespace': req.get('event-namespace'),
+            'data': req.body.toString('utf-8'),
+            'extensions': { request: req },
+        };
+        const context = {
+            'function-name': funcHandler,
+            'timeout' : timeout,
+            'runtime': process.env.FUNC_RUNTIME,
+            'memory-limit': process.env.FUNC_MEMORY_LIMIT
+        };
+        Promise.resolve(handler(event, context)).then((result) => {
+            switch(typeof result) {
+                case 'string':
+                    res.end(result);
+                    break;
+                case 'object':
+                    res.json(result);
+                    break;
+                default:
+                    res.end(JSON.stringify(result));
+            }
+            end();
+        }).catch((err) => {
+            // Catch asynchronous errors
+            handleError(err, res, funcLabel(req));
+        });
+    } catch (err) {
+        // Catch synchronous errors
+        handleError(err, res, funcLabel(req));
     }
-    _end();
-  }).catch((err) => {
-    // Catch asynchronous errors
-    _handleError(err);
-  });
-} catch (err) {
-  // Catch synchronous errors
-  _handleError(err);
-}`;
-const { vmscript, sandbox } = helper.loadFunc(modName, functionCallingCode);
+}
+
+function handleError(err, res, label) {
+    errorsCounter.labels(label).inc();
+    res.status(500).send('Internal Server Error');
+    console.error(`Function failed to execute: ${err.stack}`);
+}
+
+function funcLabel(req) {
+    return modName + '-' + req.method;
+}
 
 app.all('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  if (req.method === 'OPTIONS') {
-    // CORS preflight support (Allow any method or header requested)
-    res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-    res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers'])
-    res.end();
-  } else {
-    const funcLabel = modName + "-" + req.method;
-    const end = statistics.timeHistogram.labels(funcLabel).startTimer();
-    statistics.callsCounter.labels(funcLabel).inc();
-    const handleError = (err) => {
-      statistics.errorsCounter.labels(funcLabel).inc();
-      res.status(500).send('Internal Server Error');
-      console.error(`Function failed to execute: ${err.stack}`);
-    };
-    let data = req.body.toString('utf-8');
-    if (req.get('content-type') === 'application/json') {
-      data = JSON.parse(data)
+    res.header('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+        // CORS preflight support (Allow any method or header requested)
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        res.end();
+    } else {
+        const label = funcLabel(req);
+        const end = timeHistogram.labels(label).startTimer();
+        callsCounter.labels(label).inc();
+
+        const sandbox = Object.assign({}, global, {
+            __filename: modPath,
+            __dirname: path.dirname(modPath),
+            module: new Module(modPath),
+            require: (p) => modRequire(p, req, res, end),
+        });
+
+        try {
+            script.runInNewContext(sandbox, { timeout : timeout * 1000 });
+        } catch (err) {
+            if (err.toString().match('Error: Script execution timed out')) {
+                res.status(408).send(err);
+                // We cannot stop the spawned process (https://github.com/nodejs/node/issues/3020)
+                // we need to abruptly stop this process
+                console.error('CRITICAL: Unable to stop spawned process. Exiting');
+                process.exit(1);
+            } else {
+                handleError(err, res, funcLabel);
+            }
+        }
     }
-    const reqSandbox = Object.assign({
-      event: {
-        data,
-        'event-type': req.get('event-type'),
-        'event-id': req.get('event-id'),
-        'event-time': req.get('event-time'),
-        'event-namespace': req.get('event-namespace'),
-        extensions: {
-          request: req,
-        },
-      },
-      context: {
-        'function-name': funcHandler,
-        timeout,
-        runtime: process.env.FUNC_RUNTIME,
-        'memory-limit': process.env.FUNC_MEMORY_LIMIT,
-      },
-      _handleError: handleError,
-      _res: res,
-      _end: end,
-      process: Object.assign({}, process),
-    }, sandbox);
-    try {
-      vmscript.runInNewContext(reqSandbox, { timeout: timeout*1000 });
-    } catch (err) {
-      if (err.toString().match("Error: Script execution timed out")) {
-          res.status(408).send(err);
-          // We cannot stop the spawned process (https://github.com/nodejs/node/issues/3020)
-          // we need to abruptly stop this process
-          console.error('CRITICAL: Unable to stop spawned process. Exiting')
-          process.exit(1)
-      } else {
-        handleError(err);
-      }
-    }
-  }
 });
 
 app.listen(funcPort);
+
