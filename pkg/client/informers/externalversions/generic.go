267a7942ebf8c32a09f163cf8699566cf3df6678
Murali Reddy <muralimmreddy@gmail.com>
2018-05-15 13:56:12 +0530

    generated code with new version of code-generator

--- pkg/client/informers/externalversions/generic.go
+++ pkg/client/informers/externalversions/generic.go
@@ -20,6 +20,7 @@ package externalversions
 
 import (
 	"fmt"
+
 	v1beta1 "github.com/kubeless/kubeless/pkg/apis/kubeless/v1beta1"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	cache "k8s.io/client-go/tools/cache"
