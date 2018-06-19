c8a5c5b806f062de5dead8baeba9b79f3e994dd0
Graham Scott <grahams@brandwatch.com>
2018-06-19 13:19:11 +0100

    Update test so that stream/response manipulation happens in the userland function.

--- examples/nodejs/hellostream.js
+++ examples/nodejs/hellostream.js
@@ -1,18 +1,25 @@
-var from = require('from2')
+const from = require('from2');
+const eos = require('end-of-stream');
 
 function fromString(string) {
-  return from(function(size, next) {
-    if (string.length <= 0) return next(null, null)
+    return from(function(size, next) {
+        if (string.length <= 0) return next(null, null);
 
-    var chunk = string.slice(0, size)
-    string = string.slice(size)
+        const chunk = string.slice(0, size);
+        string = string.slice(size);
 
-    next(null, chunk)
-  })
+        next(null, chunk);
+  });
 }
 
-module.exports = {
-  foo: function (event, context) {
-    return fromString('hello world!')
-  }
+module.exports = (event, context) => {
+    return new Promise((resolve, reject) => {
+        const {res} = event.extension;
+        const stream = fromString('hello world');
+
+        eos(stream, err => err ? reject(err) : resolve(stream));
+
+        res.setHeader('Content-Type', 'text/event-stream; charset=utf-8');
+        stream.pipe(res);
+    });
 }
