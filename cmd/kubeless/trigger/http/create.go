e6f9fb996edd0ff9a7c8d304992c13e4679ccd47
Indresh Mishra <imishra@vmware.com>
2019-03-13 16:49:16 +0530

    fixing gofmt issue

--- cmd/kubeless/trigger/http/create.go
+++ cmd/kubeless/trigger/http/create.go
@@ -18,11 +18,11 @@ package http
 
 import (
 	"fmt"
-	"github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
 	httpApi "github.com/kubeless/http-trigger/pkg/apis/kubeless/v1beta1"
 	httpUtils "github.com/kubeless/http-trigger/pkg/utils"
 	kubelessUtils "github.com/kubeless/kubeless/pkg/utils"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -183,6 +183,6 @@ func init() {
 	createCmd.Flags().StringP("tls-secret", "", "", "Specify an existing secret that contains a TLS private key and certificate to secure ingress")
 	createCmd.Flags().Bool("dryrun", false, "Output JSON manifest of the function without creating it")
 	createCmd.Flags().StringP("output", "o", "yaml", "Output format")
-	createCmd.Flags().BoolP("cors-enable","",false,"If true then cors will be enabled on Http Trigger")
+	createCmd.Flags().BoolP("cors-enable", "", false, "If true then cors will be enabled on Http Trigger")
 	createCmd.MarkFlagRequired("function-name")
 }
